3a5a8544effc84d2400c5b75369463eed37f46ba
pyaephyowin <pyaephyowin.dev@gmail.com>
2024-05-14 19:23:01 +0630

    add more translations

--- src/app/[locale]/auth/employer-login/page.tsx
+++ src/app/[locale]/auth/employer-login/page.tsx
@@ -5,8 +5,10 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Link } from '@/navigation';
 import { useAuthSession } from '@/store/useAuthSession';
+import { useTranslations } from 'next-intl';
 
 export default function Login() {
+  const t = useTranslations('login');
   const { setAuth } = useAuthSession();
   const handleLogin = () => {
     setAuth({
@@ -30,7 +32,7 @@ export default function Login() {
       >
         <div className='mx-auto grid w-[350px] gap-6'>
           <div className='grid gap-2 text-center'>
-            <h1 className='text-3xl font-bold'>Employer Login</h1>
+            <h1 className='text-3xl font-bold'>{t('employer-title')}</h1>
             <p className='text-balance text-muted-foreground'>
               Enter your email below to login to your account
             </p>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,16 +2,18 @@
 
 import { Link } from '@/navigation';
 import { useAuthSession } from '@/store/useAuthSession';
+import { useTranslations } from 'next-intl';
 import LocaleSwitcher from './LocalSwitcher';
 
 export function Navbar() {
+  const t = useTranslations('navbar');
   const { session, removeAuth } = useAuthSession();
 
   return (
     <nav className='bg-gray-800'>
       <div className='container mx-auto h-16 flex items-center justify-between'>
         <Link href='/' className='flex-shrink-0 text-white'>
-          Next.js App
+          Next.js Template
         </Link>
 
         <div className='flex items-center gap-4 justify-center'>
@@ -24,7 +26,7 @@ export function Navbar() {
                   href='/employer/profile'
                   className='h-10 px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 hover:bg-gray-700'
                 >
-                  Employer Profile
+                  {t('employer-profile')}
                 </Link>
               )}
 
@@ -33,7 +35,7 @@ export function Navbar() {
                   href='/jobseeker/profile'
                   className='h-10 px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 hover:bg-gray-700'
                 >
-                  Job Seeker Profile
+                  {t('jobseeker-profile')}
                 </Link>
               )}
 
@@ -43,7 +45,7 @@ export function Navbar() {
                 }}
                 className='h-10 px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 hover:bg-gray-700'
               >
-                logout
+                {t('logout')}
               </button>
             </>
           ) : (
@@ -52,14 +54,14 @@ export function Navbar() {
                 href='/auth/employer-login'
                 className='h-10 px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 hover:bg-gray-700'
               >
-                Employer Login
+                {t('employer-login')}
               </Link>
 
               <Link
                 href='/auth/jobseeker-login'
                 className='h-10 px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 hover:bg-gray-700'
               >
-                Job Seeker Login
+                {t('jobseeker-login')}
               </Link>
             </div>
           )}
